39c1fa24851b31b4cf1e74e8e197cf1128c29b0f
Visvaldas <visva.rapalis@gmail.com>
2020-07-10 12:42:06 +0300

    Add strings examples

---
+++ Ruby/strings.rb
@@ -0,0 +1,22 @@
+print "Hello"
+print "World"
+print "Hello\n"
+print "\tWorld\n"
+
+puts "hello"
+puts "world"
+
+p "hello"
+p 'world'
+p "42"
+p 42
+
+puts "hello".length
+puts "hello"[0]
+puts "hello"[1]
+puts "Hello " + "World" + ", how are you?"
+
+puts "coding"[2]
+puts "Hey" + "Programmers"
+puts "coding".length
+puts "Hey-Programmers".length < 5
