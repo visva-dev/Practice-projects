ebec77980d494bf93ee5fbf9c93b6b9c55f75863
Visvaldas <visva.rapalis@gmail.com>
2020-07-10 11:55:57 +0300

    Add Booleans exercises & update basics_exercises

---
+++ Ruby/Booleans.rb
@@ -0,0 +1,18 @@
+puts true
+puts !true
+puts !false
+
+puts false && false
+puts false && true
+puts true && true
+puts true && false
+
+puts false || false
+puts false || true
+puts true || false
+puts true || true
+
+puts 13 % 2 == 0
+puts !false || false
+puts true && false
+puts 6 > 0 && 6 % 2 == 0
--- Ruby/basics_exercises.rb
+++ Ruby/basics_exercises.rb
@@ -1,3 +1,4 @@
+puts "Hello World" #This is a comment in Ruby
 two_words = "Boby" + " Dylan"
 puts two_words
 
@@ -35,3 +36,20 @@ puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
 puts 4.30 * 4.30
 puts 6.13 * 6.13
 puts 124.34 * 124.34
+
+puts 6 + 4 / 2
+puts (6 + 4) / 2
+puts 9 / 2
+puts 9 / 2.0
+puts 13 % 5 # 13 / 5 = 2 remainder 3
+
+puts 6 == 6
+puts 4 == 5
+puts 3 != 4
+puts 20 > 7
+puts 4 < 6
+puts 100 >= 100
+puts 200 >= 100
+puts 27 % 6
+puts 10 / 3
+puts 5 + 5 / 2.0
