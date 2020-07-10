2d6ce3bb1c6b0bace8524f0cd65d6b70e6b9497d
Visvaldas <visva.rapalis@gmail.com>
2020-07-10 13:36:10 +0300

    Add variables examples

---
+++ Ruby/variables.rb
@@ -0,0 +1,61 @@
+location = "Lithuania"
+puts location
+my_number = 30
+puts my_number
+puts my_number + 4
+my_number + 40
+puts my_number
+my_number = my_number + 40
+puts my_number
+my_number += 1
+puts my_number
+my_number -= 40
+puts my_number
+
+another_number = 4
+is_even = another_number % 2 == 0
+puts is_even
+
+sentence = "There is no spoon"
+puts "---1:"
+puts sentence + "?"
+
+puts "---2:"
+puts sentence
+
+sentence += "."
+puts "---3:"
+puts sentence
+
+num = 6 / 2
+puts "---4:"
+puts num
+
+puts "---5:"
+puts sentence[num]
+
+num = 40
+num + 5
+puts "---1:"
+puts num
+
+num += 2
+puts "---2:"
+puts num
+
+isEven = num % 2 == 0
+puts "---3:"
+puts isEven
+
+isNegative = num < 0
+puts "---4:"
+puts isNegative
+
+puts "---5:"
+puts isEven || isNegative
+
+puts "---6:"
+puts isEven && isNegative
+
+puts "---7:"
+puts isEven && !isNegative
