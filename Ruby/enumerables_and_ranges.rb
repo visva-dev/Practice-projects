aa77a18b95a90e03dd77a93567f75348ccb8d4ba
Visvaldas <visva.rapalis@gmail.com>
2020-07-13 18:55:42 +0300

    Add more enumerables examples

--- Ruby/enumerables_and_ranges.rb
+++ Ruby/enumerables_and_ranges.rb
@@ -94,3 +94,66 @@ puts '<--->'
 (0...10).each { puts "Hello!" }
 
 4.times { puts 'hi' }
+
+def to_initials(name)
+    parts = name.split(' ')
+    initials = ''
+    parts.each { |part| initials += part[0]}
+    return initials
+end
+puts to_initials('Kelvin Bridges')
+puts to_initials('Michaela Yamamoto')
+puts to_initials('Mary La Grange')
+
+def first_in_array(arr, el1, el2)
+    if arr.index(el1) < arr.index(el2)
+        return el1
+        else
+            return el2
+        end
+    end
+puts first_in_array(['a','b','c','d'], 'd','b')
+puts first_in_array(['cat', 'dog', 'horse', 'fox'], 'cat','dog')
+
+def abbreviate_sentence(sent)
+    words = sent.split(' ')
+    new_words = []
+
+    words.each do |word|
+        if word.length > 4
+            new_word = abbreviate_word(word)
+            new_words << new_word
+        else
+            new_words << word
+        end
+    end
+    new_sent = new_words.join(' ')
+end
+
+def abbreviate_word(word)
+    vowels = 'aeiou'
+    non_vowels = ''
+
+    word.each_char do |char|
+        if !vowels.include?(char)
+            non_vowels += char
+        end
+    end
+
+    return non_vowels
+end
+puts abbreviate_sentence('Follow the yellow brick road')
+puts abbreviate_sentence('what a wonderful life')
+
+def format_name(str)
+    parts = str.split(' ')
+    new_parts = []
+    parts.each do |part|
+        new_parts << part[0].upcase + part[1..-1].downcase
+    end
+    return new_parts.join(' ')
+end
+print format_name('joe tribiany')
+puts
+print format_name('chandler bing')
+puts
