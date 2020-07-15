b6d76a081842e199c7443f2feb9f1db6c9681b10
Visvaldas <visva.rapalis@gmail.com>
2020-07-15 15:53:42 +0300

    Add more ruby syntax examples

--- Ruby/stylistic_features_of_Ruby.rb
+++ Ruby/stylistic_features_of_Ruby.rb
@@ -82,3 +82,87 @@ end
 def all_numbers_even?(nums)
     nums.all? { |num| num.even? }
 end
+
+# Symbols
+# Ruby has an additional data type that is similar to Strings, called Symbols.
+str = "hello"   # the string 
+sym = :hello    # the symbol
+
+p str.length    # => 5
+p sym.length    # => 5
+
+p str[1]        # => "e"
+p sym[1]        # => "e"
+
+p str == sym    # => false
+# a string is different from a symbol!
+
+# Symbols are Immutable
+str = "hello"
+sym = :hello
+
+str[0] = "x"
+sym[0] = "x"
+
+p str   # => "xello"
+p sym   # => :hello
+
+# Symbols requires less memory
+"hello".object_id   # => 70233443667980
+"hello".object_id   # => 70233443606440
+"hello".object_id   # => 70233443438700
+
+:hello.object_id    # => 2899228
+:hello.object_id    # => 2899228
+:hello.object_id    # => 2899228
+
+# Symbols as hash keys
+my_bootcamp = { :name=>"App Academy", :color=>"red", :locations=>["NY", "SF", "ONLINE"] }
+p my_bootcamp           # => {:name=>"App Academy", :color=>"red", :locations=>["NY", "SF", "ONLINE"]}
+p my_bootcamp[:color]   #=> "red
+
+# When initializing a hash with symbol keys, Ruby offers a shortcut. We can drop the rocket (=>) and move the colon (:) to the right of the symbol:
+my_bootcamp = { name:"App Academy", color:"red", locations:["NY", "SF", "ONLINE"] }
+p my_bootcamp           # => {:name=>"App Academy", :color=>"red", :locations=>["NY", "SF", "ONLINE"]}
+p my_bootcamp[:color]   #=> "red
+
+
+# Default Arguments
+# Let's make num an optional parameter.
+# By default, num will have the value of 1
+def repeat(message, num=1)
+    message * num
+end
+
+p repeat("hi") # => "hi"
+p repeat("hi", 3) # => "hihihi"
+
+# Splat Operator
+# Using splat to accept additional arguments
+def method(arg_1, arg_2, *other_args)
+    p arg_1         # "a"
+    p arg_2         # "b"
+    p other_args    # ["c", "d", "e"]
+end
+
+method("a", "b", "c", "d", "e") 
+
+# Using splat to decompose an array
+def greet(first_name, last_name)
+    p "Hey " + first_name + ", your last name is " + last_name
+end
+
+names = ["Grace", "Hopper"]
+greet(*names)    # => "Hey Grace, your last name is Hopper"
+
+######
+arr_1 = ["a", "b"]
+arr_2 = ["d", "e"]
+arr_3 = [ *arr_1, "c", *arr_2 ]
+p arr_3 # => ["a", "b", "c", "d", "e"]
+
+# Using splat to decompose a hash
+# We can use a double splat (**) to perform a similar unpacking of a hash's key-value pairs. Double splat will only work with hashes where the keys are symbols:
+old_hash = { a: 1, b: 2 }
+new_hash = { **old_hash, c: 3 }
+p new_hash # => {:a=>1, :b=>2, :c=>3}
