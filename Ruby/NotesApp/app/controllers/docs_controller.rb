dad54b8339b0716f8e5787703d1e3c8e257f0b12
Visvaldas <visva.rapalis@gmail.com>
2020-08-02 21:42:23 +0300

    Fix index.html error, add Edit and Destroy Docs

--- Ruby/NotesApp/app/controllers/docs_controller.rb
+++ Ruby/NotesApp/app/controllers/docs_controller.rb
@@ -26,15 +26,22 @@ class DocsController < ApplicationController
     end
 
     def update
+        if @doc.update(doc_params)
+            redirect_to @doc
+        else
+            render 'edit'
+        end
     end
 
     def destroy
+        @doc.destroy
+        redirect_to docs_path
     end
 
     private
     
     def find_doc
-        @docs = Doc.find(params[:id])
+        @doc = Doc.find(params[:id])
     end
     
     def doc_params
