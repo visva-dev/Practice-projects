19399462bb619b7b87dbd1f0b14f9fbf8d352c57
Visvaldas <visva.rapalis@gmail.com>
2020-08-02 22:10:20 +0300

    Association between User and Files

--- Ruby/NotesApp/db/schema.rb
+++ Ruby/NotesApp/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_185312) do
+ActiveRecord::Schema.define(version: 2020_08_02_190023) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
